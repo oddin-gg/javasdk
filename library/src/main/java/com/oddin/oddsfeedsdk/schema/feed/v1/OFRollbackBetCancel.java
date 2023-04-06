13a7a8d91a04703e8bfa8d0d98a4af2cf11e1151
Dušan Saiko <dusan@saiko.cz>
2023-04-06 17:06:16 +0200

    Fixing ignored files

---
+++ library/src/main/java/com/oddin/oddsfeedsdk/schema/feed/v1/OFRollbackBetCancel.java
@@ -0,0 +1,88 @@
+package com.oddin.oddsfeedsdk.schema.feed.v1;
+
+
+import com.oddin.oddsfeedsdk.mq.entities.BasicMessage;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "market"
+})
+@XmlRootElement(name = "rollback_bet_cancel")
+public class OFRollbackBetCancel implements BasicMessage {
+
+    @XmlElement(required = true)
+    protected List<OFRollbackBetCancelMarket> market;
+    @XmlAttribute(name = "product", required = true)
+    protected int product;
+    @XmlAttribute(name = "event_id", required = true)
+    protected String eventId;
+    @XmlAttribute(name = "timestamp", required = true)
+    protected long timestamp;
+
+    public List<OFRollbackBetCancelMarket> getMarket() {
+        if (market == null) {
+            market = new ArrayList<>();
+        }
+        return this.market;
+    }
+
+    /**
+     * Gets the value of the product property.
+     *
+     */
+    public int getProduct() {
+        return product;
+    }
+
+    /**
+     * Sets the value of the product property.
+     *
+     */
+    public void setProduct(int value) {
+        this.product = value;
+    }
+
+    /**
+     * Gets the value of the eventId property.
+     *
+     * @return
+     *     possible object is
+     *     {@link String }
+     *
+     */
+    public String getEventId() {
+        return eventId;
+    }
+
+    /**
+     * Sets the value of the eventId property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *
+     */
+    public void setEventId(String value) {
+        this.eventId = value;
+    }
+
+    /**
+     * Gets the value of the timestamp property.
+     *
+     */
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * Sets the value of the timestamp property.
+     *
+     */
+    public void setTimestamp(long value) {
+        this.timestamp = value;
+    }
+}
